052cf51bfdbc9d4492bc72c321ed191e1b864f2e
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-03-24 21:48:14 +0600

    mongoose installed and console added

--- web/index.js
+++ web/index.js
@@ -24,10 +24,13 @@ const STATIC_PATH =
 
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/your-database-name";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log(err));
 
 // Start Define TIP API Env
 const TIP_HOST = process.env.TIP_HOST;
