dcf6b0276b7a09c6ae50b70c530448e6da113232
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-06-11 02:16:00 +0600

    dotenv added to tip page

--- web/tip-consultations.js
+++ web/tip-consultations.js
@@ -1,3 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const TIP_HOST = process.env.TIP_HOST;
+const TIP_API_VERSION = process.env.TIP_API_VERSION;
+const TIP_CLIENT_ID = process.env.TIP_CLIENT_ID;
+const TIP_TOKEN = process.env.TIP_TOKEN;
+const tip_header = {
+  Client: TIP_CLIENT_ID,
+  Authorization: "Bearer " + TIP_TOKEN,
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
 function submitConsultancy(data_consultancy, data_medical, data_order) {
   const form_data = {
     uuid: "IPS-C" + data_order?.line_items_uuid,
