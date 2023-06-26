81590100e05e29a9351b8db2851637ebbbef5f0f
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-06-27 03:11:04 +0600

    adding order code - fix

--- web/webhook-handlers.js
+++ web/webhook-handlers.js
@@ -183,7 +183,7 @@ export default {
       const ordersPaid = JSON.parse(body);
       OrderSubmit(ordersPaid);
       const line_items_uuid = ordersPaid?.line_items[0]?.properties[0]?.value;
-      console.log("## WEBHOOK: Paid order UUID:", ordersPaid);
+      console.log("## WEBHOOK: Paid order UUID:", JSON.stringify(ordersPaid));
       // console.log("## IDs of all received webhooks:");
       // console.log(receivedWebhooks);
     },
