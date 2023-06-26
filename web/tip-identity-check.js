b583002bb94e92d0235b9862c775955adbda26a4
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-06-26 09:35:55 +0600

    minor fixing on identity check request 1

--- web/tip-identity-check.js
+++ web/tip-identity-check.js
@@ -41,6 +41,9 @@ function identityCheck(data_medical, data_order) {
     ).then((response) => response.json());
 
     console.log("POST: APP / identityCheckPayload");
+    console.log("client id:", TIP_CLIENT_ID);
+    console.log("jwt:", TIP_TOKEN);
+    console.log("full header", tip_header);
     console.log("identityCheckPayload:", JSON.stringify(identityCheckPayload));
 
     if (response.status === 200) {
