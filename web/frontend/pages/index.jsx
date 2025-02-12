ae145d9f67e09fc3904b5a2bb83d8981705e75b0
Mozahidur Rahman <mr.rousnay@gmail.com>
2025-02-13 04:55:28 +0600

    TIP ordering system modification 7

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -57,7 +57,7 @@ export default function HomePage() {
 
   return (
     <Page narrowWidth>
-      <TitleBar title="App name" primaryAction={null} />
+      <TitleBar title="TIP Consultancy APP" primaryAction={null} />
       <Layout>
         <Layout.Section>
           <Card sectioned>
@@ -121,12 +121,67 @@ export default function HomePage() {
                 </Text>
               ))}
 
+              <Text variant="bodyMd" fontWeight="bold">Consultation Info:</Text>
+              <Text>Treatment IDs: {order.consultation_data.submitted.map((item, index) => (
+                <span key={index}>{item.consultancy_data.treatment}, </span>
+              ))}</Text>
+              <Text>Require for approval: {order.approval_required_item_count}</Text>
+              <Text>Approved by TIP: {order.approved_item_count}</Text>
+
+              <Text variant="bodyMd" fontWeight="bold">Patient Info:</Text>
+              <Text>Phone: {order?.patient_info?.phone}</Text>
+              <Text>Gender: {order?.patient_info?.gender}</Text>
+              <Text>Date of Birth: {order?.patient_info?.dob}</Text>
+
               <Text variant="bodyMd" fontWeight="bold">Billing Address:</Text>
               <Text>{order.billing_address.address1}, {order.billing_address.city}, {order.billing_address.county} {order.billing_address.postcode}</Text>
 
               <Text variant="bodyMd" fontWeight="bold">Shipping Address:</Text>
               <Text>{order.shipping_address.address1}, {order.shipping_address.city}, {order.shipping_address.county} {order.shipping_address.postcode}</Text>
             </Card>
+
+            <Card title="Identity Data">
+              <Card.Section>
+                <Text variant="bodyMd" fontWeight="bold">Submitted:</Text>
+                <pre>{JSON.stringify(order.identity_data.submitted, null, 2)}</pre>
+              </Card.Section>
+              <Card.Section>
+                <Text variant="bodyMd" fontWeight="bold">Notification:</Text>
+                <pre>{JSON.stringify(order.identity_data.notification, null, 2)}</pre>
+              </Card.Section>
+            </Card>
+
+            {order.consultation_data.submitted.map((item, index) => (
+              <Card key={index} title={`Consultation Data: ${index + 1}`}>
+                <Card.Section>
+                  <Text variant="bodyMd" fontWeight="bold">Submitted:</Text>
+                  <pre>{JSON.stringify(order.consultation_data.submitted[index], null, 2)}</pre>
+                </Card.Section>
+                <Card.Section>
+                  <Text variant="bodyMd" fontWeight="bold">Notification:</Text>
+                  <pre>{JSON.stringify(order.consultation_data.notification[index], null, 2)}</pre>
+                </Card.Section>
+              </Card>
+            ))}
+
+            {order.order_data.submitted != null ? (
+              <Card title="Order Data">
+                <Card.Section>
+                  <Text variant="bodyMd" fontWeight="bold">Submitted:</Text>
+                  <pre>{JSON.stringify(order.order_data.submitted, null, 2)}</pre>
+                </Card.Section>
+                <Card.Section>
+                  <Text variant="bodyMd" fontWeight="bold">Notification:</Text>
+                  <pre>{JSON.stringify(order.order_data.notification, null, 2)}</pre>
+                </Card.Section>
+              </Card>
+            ) : (
+              <Card title="The raw order data stored in the database">
+                <Card.Section>
+                  <pre>{order.order_data.order_info ? JSON.stringify(order.order_data.order_info, null, 2) : "No order data available"}</pre>
+                </Card.Section>
+              </Card>
+            )}
           </Layout.Section>
         )}
       </Layout>
